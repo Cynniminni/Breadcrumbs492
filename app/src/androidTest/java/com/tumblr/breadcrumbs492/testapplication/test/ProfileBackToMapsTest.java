4a89a4e03f5c4968d89bb9c30f30c3afd48c25e7
ARGutierrez <argutierrez92@gmail.com>
2015-05-13 00:52:45 -0700

    Added new unit tests for back button

--- app/src/androidTest/java/com/tumblr/breadcrumbs492/testapplication/test/ProfileBackToMapsTest.java
+++ app/src/androidTest/java/com/tumblr/breadcrumbs492/testapplication/test/ProfileBackToMapsTest.java
@@ -4,20 +4,23 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
 import android.test.FlakyTest;
 import android.test.InstrumentationTestCase;
+import android.widget.LinearLayout;
 
 import com.robotium.solo.Solo;
 import com.tumblr.breadcrumbs492.testapplication.LoginActivity;
+import com.tumblr.breadcrumbs492.testapplication.MapsActivity;
+import com.tumblr.breadcrumbs492.testapplication.ProfileActivity;
 import com.tumblr.breadcrumbs492.testapplication.R;
 import com.tumblr.breadcrumbs492.testapplication.SettingsActivity;
 
 /**
- * Created by Aaron on 5/11/2015.
+ * Created by Aaron on 5/13/2015.
  */
-public class LoginLogoutTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+public class ProfileBackToMapsTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 
     private Solo solo;
 
-    public LoginLogoutTest() {
+    public ProfileBackToMapsTest() {
         super(LoginActivity.class);
     }
 
@@ -53,30 +56,16 @@ public class LoginLogoutTest extends ActivityInstrumentationTestCase2<LoginActiv
         solo.clickOnView(solo.getView(R.id.signin));
         //Wait for activity: 'com.tumblr.breadcrumbs492.testapplication.MapsActivity'
         assertTrue("com.tumblr.breadcrumbs492.testapplication.MapsActivity is not found!", solo.waitForActivity(com.tumblr.breadcrumbs492.testapplication.MapsActivity.class, 2000));
+
+        solo.sleep(1000);
+        solo.clickOnImageButton(0);
+
+        assertTrue("com.tumblr.breadcrumbs492.testapplication.MapsActivity is not found!", solo.waitForActivity(ProfileActivity.class, 2000));
         // Log out
         solo.clickOnActionBarHomeButton();
-        solo.clickOnActionBarItem(R.id.action_logout_maps);
-        //Wait for activity: 'com.tumblr.breadcrumbs492.testapplication.LoginActivity'
-        //
-        assertTrue("com.tumblr.breadcrumbs492.testapplication.LoginActivity is not found!", solo.waitForActivity(LoginActivity.class));
+        solo.clickOnActionBarItem(R.id.go_back);
+        assertTrue("com.tumblr.breadcrumbs492.testapplication.MapsActivity is not found!", solo.waitForActivity(MapsActivity.class, 2000));
 
-        // Log back in
-        //Click on Empty Text View
-        solo.clickOnView(solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_username));
-        //Enter the text: 'daniel'
-        solo.clearEditText((android.widget.EditText) solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_username));
-        solo.enterText((android.widget.EditText) solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_username), "scotttak");
-        //Click on Empty Text View
-        solo.clickOnView(solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_password));
-        //Enter the text: 'password1'
-        solo.clearEditText((android.widget.EditText) solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_password));
-        solo.enterText((android.widget.EditText) solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_password), "password1");
-        //Press next button
-        solo.pressSoftKeyboardNextButton();
-        //Click on Sign in or register
-        solo.clickOnView(solo.getView(R.id.signin));
-        //Wait for activity: 'com.tumblr.breadcrumbs492.testapplication.MapsActivity'
-        assertTrue("com.tumblr.breadcrumbs492.testapplication.MapsActivity is not found!", solo.waitForActivity(com.tumblr.breadcrumbs492.testapplication.MapsActivity.class, 2000));
 
     }
 }
--- app/src/androidTest/java/com/tumblr/breadcrumbs492/testapplication/test/RandomCrumbTest.java
+++ app/src/androidTest/java/com/tumblr/breadcrumbs492/testapplication/test/RandomCrumbTest.java
@@ -1,23 +1,22 @@
 package com.tumblr.breadcrumbs492.testapplication.test;
 
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.ApplicationTestCase;
 import android.test.FlakyTest;
-import android.test.InstrumentationTestCase;
 
 import com.robotium.solo.Solo;
 import com.tumblr.breadcrumbs492.testapplication.LoginActivity;
+import com.tumblr.breadcrumbs492.testapplication.MapsActivity;
+import com.tumblr.breadcrumbs492.testapplication.ProfileActivity;
 import com.tumblr.breadcrumbs492.testapplication.R;
-import com.tumblr.breadcrumbs492.testapplication.SettingsActivity;
 
 /**
- * Created by Aaron on 5/11/2015.
+ * Created by a4r0ng on 5/13/2015.
  */
-public class LoginLogoutTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+public class RandomCrumbTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 
     private Solo solo;
 
-    public LoginLogoutTest() {
+    public RandomCrumbTest() {
         super(LoginActivity.class);
     }
 
@@ -53,31 +52,20 @@ public class LoginLogoutTest extends ActivityInstrumentationTestCase2<LoginActiv
         solo.clickOnView(solo.getView(R.id.signin));
         //Wait for activity: 'com.tumblr.breadcrumbs492.testapplication.MapsActivity'
         assertTrue("com.tumblr.breadcrumbs492.testapplication.MapsActivity is not found!", solo.waitForActivity(com.tumblr.breadcrumbs492.testapplication.MapsActivity.class, 2000));
-        // Log out
-        solo.clickOnActionBarHomeButton();
-        solo.clickOnActionBarItem(R.id.action_logout_maps);
-        //Wait for activity: 'com.tumblr.breadcrumbs492.testapplication.LoginActivity'
-        //
-        assertTrue("com.tumblr.breadcrumbs492.testapplication.LoginActivity is not found!", solo.waitForActivity(LoginActivity.class));
 
-        // Log back in
-        //Click on Empty Text View
-        solo.clickOnView(solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_username));
-        //Enter the text: 'daniel'
-        solo.clearEditText((android.widget.EditText) solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_username));
-        solo.enterText((android.widget.EditText) solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_username), "scotttak");
-        //Click on Empty Text View
-        solo.clickOnView(solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_password));
-        //Enter the text: 'password1'
-        solo.clearEditText((android.widget.EditText) solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_password));
-        solo.enterText((android.widget.EditText) solo.getView(com.tumblr.breadcrumbs492.testapplication.R.id.enter_password), "password1");
-        //Press next button
-        solo.pressSoftKeyboardNextButton();
-        //Click on Sign in or register
-        solo.clickOnView(solo.getView(R.id.signin));
-        //Wait for activity: 'com.tumblr.breadcrumbs492.testapplication.MapsActivity'
-        assertTrue("com.tumblr.breadcrumbs492.testapplication.MapsActivity is not found!", solo.waitForActivity(com.tumblr.breadcrumbs492.testapplication.MapsActivity.class, 2000));
+        solo.sleep(1000);
+        solo.clickOnImageButton(3);
+        solo.sleep(1000);
+        solo.clickOnImageButton(3);
+        solo.sleep(1000);
+        solo.clickOnImageButton(3);
+        solo.sleep(1000);
+        solo.clickOnImageButton(3);
+        solo.sleep(1000);
+        solo.clickOnImageButton(3);
+
+        assertTrue("com.tumblr.breadcrumbs492.testapplication.MapsActivity is not found!", solo.waitForActivity(MapsActivity.class, 2000));
+
 
     }
 }
-
